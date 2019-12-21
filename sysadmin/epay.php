0a66f0c4593997514612ed8cd121791935fe5001
NetherXiaoYu <2031464675@qq.com>
2019-12-21 13:06:05 +0800

    修复无法修改手续费问题

--- sysadmin/epay.php
+++ sysadmin/epay.php
@@ -39,11 +39,23 @@ include("./head.php");
                                         </div>
                                         <div class="form-group">
                                             <label for="exampleInputEmail1">易支付ID</label>
-                                            <input name="epayid" type="text" class="form-control" id="epayid" placeholder="公告标题" value="<?=$conf['epayid']?>">
+                                            <input name="epayid" type="text" class="form-control" id="epayid" placeholder="易支付ID" value="<?=$conf['epayid']?>">
                                         </div>
                                         <div class="form-group">
                                             <label for="exampleInputEmail1">易支付KEY</label>
-                                            <input name="epaykey" type="text" class="form-control" id="epaykey" placeholder="公告标题" value="<?=$conf['epaykey']?>">
+                                            <input name="epaykey" type="text" class="form-control" id="epaykey" placeholder="易支付KEY" value="<?=$conf['epaykey']?>">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="exampleInputEmail1">微信通道手续费（1%填1）</label>
+                                            <input name="epay_fee_wx" type="text" class="form-control" id="epayurl" placeholder="微信通道手续费" value="<?=$conf['epay_fee_wx']?>">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="exampleInputEmail1">QQ通道手续费（1%填1）</label>
+                                            <input name="epay_fee_qq" type="text" class="form-control" id="epayurl" placeholder="QQ通道手续费" value="<?=$conf['epay_fee_qq']?>">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="exampleInputEmail1">支付宝通道手续费（1%填1）</label>
+                                            <input name="epay_fee_zfb" type="text" class="form-control" id="epayurl" placeholder="支付宝通道手续费" value="<?=$conf['epay_fee_zfb']?>">
                                         </div>
                                         <button type="submit" class="btn btn-default">修改</button>
                                     </form>
